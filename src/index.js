6d9f2a445e47f6b279329d09e177fcab3d312a46
Yoriiis <joris.daniel@gmail.com>
2021-08-30 19:56:52 +0200

    Add source

---
+++ src/index.js
@@ -0,0 +1,33 @@
+export default function asyncCallReducer({
+	key = 'asyncCallReducer',
+	storage = 'sessionStorage',
+	callback
+}) {
+	return new Promise(function (resolve) {
+		const dataKey = `${key}Data`;
+		const loadingKey = `${key}Loading`;
+		const dataAvailableKey = `${key}DataAvailable`;
+
+		if (window[storage].getItem(dataKey)) {
+			resolve(JSON.parse(window[storage].getItem(dataKey)));
+		} else if (window[storage].getItem(loadingKey)) {
+			document.addEventListener(dataAvailableKey, function (e) {
+				return resolve(e.detail.data);
+			});
+		} else if (!window[storage].getItem(loadingKey)) {
+			window[storage].setItem(loadingKey, true);
+			callback().then(function (data) {
+				window[storage].setItem(dataKey, JSON.stringify(data));
+				document.dispatchEvent(
+					new window.CustomEvent(dataAvailableKey, {
+						detail: {
+							data
+						}
+					})
+				);
+				resolve(data);
+				window[storage].removeItem(loadingKey);
+			});
+		}
+	});
+}
